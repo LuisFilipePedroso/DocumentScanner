43c0502cf2851f686f2609a5706ef65b156fb0e1
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-04-08 20:38:31 -0300

    feat: initial structure of Home screen

---
+++ DocumentScanner/View/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  DocumentScanner
+//
+//  Created by Luis Filipe Pedroso on 08/04/25.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
