be60949ec204da59908d323529a50716dbcc7dd7
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-04-14 10:22:33 -0300

    feat: add new fields

--- DocumentScanner/Model/Document.swift
+++ DocumentScanner/Model/Document.swift
@@ -13,6 +13,9 @@ class Document {
     var createdAt: Date = Date()
     @Relationship(deleteRule: .cascade, inverse: \DocumentPage.document)
     var pages: [DocumentPage]?
+    var isLocked: Bool = false
+    /// For Zoom Transition
+    var uniqueViewID: String = UUID().uuidString
     
     init(name: String, pages: [DocumentPage]? = nil) {
         self.name = name
